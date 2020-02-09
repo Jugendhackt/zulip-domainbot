7cbd489f95c2976ced24ff9fb55f8ec3bf8fbf7c
Lars K <lars@kroenner.eu>
2020-02-09 14:28:27 +0100

    Some db stuff

--- src/DB.py
+++ src/DB.py
@@ -1,10 +1,18 @@
 import json
+from pymongo import MongoClient
 
 class DB:
-    def start:
-        cfg = readConfig()
 
-    def readConfig():
+    def __init__(self):
+        self.client = None
+        self.db = None
+
+    def start(self):
+        cfg = self.readConfig()
+        self.__client = MongoClient(host=cfg.location, port=cfg.port, username=cfg.user, password=cfg.password, authSource=cfg.authSource, authMechanism=cfg.authMechanism)
+        self.db = self.__client.asb
+
+    def readConfig(self):
         f = open("mongocfg", "r")
-        cfg = json.loads(f.read()))
+        cfg = json.loads(f.read())
         return cfg
